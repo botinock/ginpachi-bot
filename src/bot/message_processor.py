06c2a0b96c32bd8435bfd62f0a334dd5fbfa455c
Kyr <kirillreznikovkirill@gmail.com>
2025-11-23 05:40:13 +0200

    fix quote bug

--- src/bot/message_processor.py
+++ src/bot/message_processor.py
@@ -6,7 +6,7 @@ class MessageProcessor:
     @staticmethod
     def process_message(message: Message, command: CommandObject) -> str:
         text_arg = command.args
-        quote_text = message.quote.text if message.reply_to_message else None
+        quote_text = message.quote.text if message.quote else None
         reply_text = message.reply_to_message.text if message.reply_to_message else None
         text = text_arg or quote_text or reply_text
         return text
